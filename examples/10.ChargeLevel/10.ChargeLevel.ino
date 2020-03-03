55db1c5434031b9a602efc816d283bf981a93c5c
Matthew3000 <mk@amperka.ru>
2020-03-03 16:38:22 +0300

    Update 10.ChargeLevel.ino

--- examples/10.ChargeLevel/10.ChargeLevel.ino
+++ examples/10.ChargeLevel/10.ChargeLevel.ino
@@ -3,7 +3,7 @@
 TroykaLedMatrix matrix;
 
 byte volume[8] = 
-    { 0x80, 0xc0, 0xe0, 0xf0, 0xf8, 0xfc, 0xfe, 0xff };
+    {0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff};
 byte diagram[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 float R1 = 10000;
 float R2 = 36000;
@@ -16,10 +16,10 @@ void setup() {
 void loop() {
   delay(200);
   int adcValue = analogRead(A1);
-  float V = adcValue * 5.0 / 1023.0;
+  float V = adcValue * 4.2 / 1023.0;
   float Vbat = V * (R1 + R2) / R2;
 
-  int matrixValue = (Vbat - 3.2) * 8;
+  int matrixValue = (Vbat - 2.5) * 8;
   for (int i = 0; i < 8; i++) {
     if (i < matrixValue) {
       diagram[i] = volume[i];
@@ -33,6 +33,6 @@ void loop() {
 /*
 (5,6) Заводим массив для хранения элементов «лесенки», обозначающей уровень заряда аккумулятора.
 (7-9) Определяем сопротивления R1 и R2.
-(18-20) Считываем аналоговое значение с пина A1, преобразуем по формулам и выводим в Монитор порта.
+(18-20) Считываем аналоговое значение с пина A1 и преобразуем по формулам.
 (22-30) Преобразуем напряжение в столбики на матрице, выводим изображение.
 */
